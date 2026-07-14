9f55fe08c40acf46e22e9b26e59779401caa4e74
TegarCode <oktaviantotegar2@gmail.com>
2026-07-14 16:52:00 +0700

    Register Laravel default providers on Vercel

--- Porto/backend/bootstrap/app.php
+++ Porto/backend/bootstrap/app.php
@@ -8,9 +8,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
+use Illuminate\Support\DefaultProviders;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders((new DefaultProviders)->toArray())
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
